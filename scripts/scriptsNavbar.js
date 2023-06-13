9768489777687d8a41772d7e2aa7db3381f95d55
Luan <luan.schmitz@alunos.bpkedu.com.br>
2023-06-12 21:20:53 -0300

    Links e Logos atualizadas em vetor no arquivo script navegar js

---
+++ scripts/scriptsNavbar.js
@@ -0,0 +1,39 @@
+const linksNavbar = [
+  {
+    link: "#sobre",
+    nome: "Sobre a CPA",
+  },
+  {
+    link: "#objetivos",
+    nome: "Objetivos",
+  },
+  {
+    link: "#membros",
+    nome: "Membros da CPA",
+  },
+  {
+    link: "#resultados",
+    nome: "Resultados",
+  },
+  {
+    link: "#faq",
+    nome: "FAQ",
+  },
+  {
+    link: "#legislação",
+    nome: "Legislação",
+  },
+];
+
+const navLogos = [
+  {
+    imgSrc: "../assets/logoCPA.svg",
+    alt: "Logo CPA",
+  },
+  {
+    imgSrc: "../assets/logoBPK.svg",
+    alt: "Logo Biopark",
+  },
+];
+
+
