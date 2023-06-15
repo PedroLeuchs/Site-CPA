3db9eb643ef28b71db0c6b90fffbf95e2bb0ce63
Luan <luan.schmitz@alunos.bpkedu.com.br>
2023-06-14 21:29:22 -0300

    objetivos feitos e oque é a cpa feito

--- scripts/scriptsNavbar.js
+++ scripts/scriptsNavbar.js
@@ -36,4 +36,25 @@ const navLogos = [
   },
 ];
 
+function renderLogos(logo) {
+  const logos = document.querySelector(".logoscpa");
+  const img1 = document.createElement("img");
+  img1.src = logo.imgSrc;
+  img1.alt = logo.alt;
+  logos.appendChild(img1);
+}
+function renderLogos(link) {
+  const links = document.querySelector(".links");
+  const link = document.createElement("a");
+  links.appendChild(link);
+}
+function createLogos () {
+  navLogos.forEach ((logo)=>{
+  renderLogos(logo);
+  })
+}
 
+// função para renderizar os cards;
+document.addEventListener("DOMContentLoaded", function () {
+  createLogos() 
+});
