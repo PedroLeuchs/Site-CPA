62636fb8f21db3835aa36ecef3bb120da61af1ae
victor hugo <victor.goioere@hotmail.com>
2023-06-17 15:11:19 -0300

    footer

---
+++ scripts/scriptFooter.js
@@ -0,0 +1,14 @@
+const contatoFooter = [
+    {
+    imgSrc:"https://bpkedu.com.br/wp-content/uploads/2021/08/Vector-1.svg",
+   alt: "EmailBPK",
+   mail: "cpa@bpkedu.com.br",
+   texto: "cpa@bpkedu.com.br",
+},
+  {imgSrc: "https://bpkedu.com.br/wp-content/uploads/2023/05/whatsapp-1.svg",
+  alt: "WhatsBPK"
+  texto:
+
+
+}
+]
